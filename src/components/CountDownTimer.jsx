342512eaac750c031bc3f924fe1a13d10a346db2
tejas-vivek <tejas.helwatkar@gmail.com>
2025-03-30 23:00:59 -0400

    Add timer logic

--- src/components/CountDownTimer.jsx
+++ src/components/CountDownTimer.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Timer from "./Timer";
+import { BsFillPlayFill, BsPauseFill, BsStopFill } from "react-icons/bs";
 
 export default function CountDownTimer() {
   const [hours, setHours] = useState(0);
@@ -9,23 +11,91 @@ export default function CountDownTimer() {
 
   useEffect(() => {
     let interval;
-    if(isRunning){
-        interval = setInterval(()=>{
-            if(milisec > 0){
-                setMilisec((milisec) => milisec - 1);
-            } else if(sec > 0){
-                setSec((sec) => sec -1);
-                setMilisec(99);
-            }
-        });
+    if (isRunning) {
+      interval = setInterval(() => {
+        if (milisec > 0) {
+          setMilisec((milisec) => milisec - 1);
+        } else if (sec > 0) {
+          setSec((sec) => sec - 1);
+          setMilisec(99);
+        } else if (mins > 0) {
+          setMins((mins) => mins - 1);
+          setSec(59);
+          setMilisec(99);
+        } else if (hours > 0) {
+          setHours((hours) => hours - 1);
+          setMins(59);
+          setSec(59);
+          setMilisec(99);
+        }
+      }, 10);
     }
     return () => clearInterval(interval);
-  }, [milisec]);
+  }, [milisec, sec, mins, hours, isRunning]);
 
+  //Start timers
+  function startTimer(){
+    if(hours !== 0 || mins !== 0 || sec !== 0 || milisec !== 0){
+        setIsRunning(true);
+    } else {
+        window.alert("Add time");
+    }
+  }
+
+  //Pause timer
+  function pauseTimer(){
+    setIsRunning(false);
+  }
+
+  function stopTimer(){
+    resetTimer();
+  }
+
+  function resetTimer(){
+    setIsRunning(false);
+    setMilisec(0);
+    setSec(0);
+    setMins(0);
+    setHours(0);
+  }
+
+  // Handlers
+  const changeSeconds = (e) => {
+    setSec(e.target.value);
+  };
+  const changeMinutes = (e) => {
+    setMins(e.target.value);
+  };
+  const changeHours = (e) => {
+    setHours(e.target.value);
+  };
   return (
     <div>
       <h1 className="title">Restart your countdown</h1>
-      <div>CountDownTimer</div>
+      <Timer
+        milisec={milisec}
+        sec={sec}
+        mins={mins}
+        hours={hours}
+        changeSeconds={changeSeconds}
+        changeMinutes={changeMinutes}
+        changeHours={changeHours}
+      />
+      <br />
+
+      {!isRunning && (
+        <button className="btn btn-accept btn-lg" onClick={startTimer}>
+          <BsFillPlayFill />
+        </button>
+      )}
+      {isRunning && (
+        <button className="btn btn-warning btn-lg" onClick={pauseTimer}>
+          <BsPauseFill />
+        </button>
+      )}
+      <button className="btn btn-danger btn-lg" onClick={stopTimer}>
+        <BsStopFill />
+      </button>
     </div>
   );
 }
